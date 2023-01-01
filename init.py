4032233b671787f2ab0022ccd9cb4fb5045978fb
Devin Slick <github@devinslick.com>
2023-01-01 13:34:29 -0600

    Move lojack_tracker.py to custom_components

--- init.py
+++ init.py
@@ -1,4 +1,4 @@
-from .lojack_tracker import LoJackDeviceTracker
+from .custom_components.lojack_tracker import LoJackDeviceTracker
 
 def setup(hass, config):
     return LoJackDeviceTracker(hass, config)
